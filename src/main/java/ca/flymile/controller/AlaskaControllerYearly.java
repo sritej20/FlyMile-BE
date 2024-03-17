65ddc5e3765be5392947253fcefbb9dea861d040
russ.aulakh <russ.aulakh@gmail.com>
2024-03-17 12:34:32 -0400

    Added entire year's daily cheapest price data

---
+++ src/main/java/ca/flymile/controller/AlaskaControllerYearly.java
@@ -0,0 +1,54 @@
+package ca.flymile.controller;
+
+
+import ca.flymile.ModelAlaska30Days.dailyCheapest;
+import ca.flymile.service.AlaskaYearly;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static ca.flymile.InputValidation.InputValidation.validateFlightSearchParamsForAlaska30Days;
+
+/**
+ * The AlaskaControllerYearly class handles HTTP requests related to flight data retrieval for a yearly period from the Alaska Airlines website.
+ */
+@RestController
+@RequestMapping("/flights/alaska/yearly")
+@CrossOrigin(origins = "*")
+public class AlaskaControllerYearly {
+
+    private final AlaskaYearly alaskaYearly;
+
+    /**
+     * Constructs a new AlaskaControllerYearly with the specified service for Alaska Airlines yearly cheapest price & point combo.
+     *
+     * @param alaskaYearly The service responsible for retrieving flight data from the Alaska Airlines for yearly cheapest price & point combo.
+     */
+    @Autowired
+    public AlaskaControllerYearly(AlaskaYearly alaskaYearly) {
+        this.alaskaYearly = alaskaYearly;
+    }
+
+    /**
+     * Retrieves a list of flight data for an entire year based on the provided search parameters.
+     *
+     * @param departure The departure airport code.
+     * @param arrival   The arrival airport code.
+     * @return A CompletableFuture of a list of dailyCheapest objects, each representing a date with available pricing details within a yearly period.
+     */
+    @GetMapping
+    public CompletableFuture<List<dailyCheapest>> getFlightDataListYearly(
+            @RequestParam String departure,
+            @RequestParam String arrival
+    ) {
+        // Validate the search parameters
+        validateFlightSearchParamsForAlaska30Days(departure.toUpperCase(), arrival.toUpperCase(), LocalDate.now().toString());
+
+        // Retrieve and return the daily Cheapest List for the year
+        return alaskaYearly.getFlightDataListAlaskaYearly(departure, arrival);
+    }
+}
+
---
+++ src/main/java/ca/flymile/service/AlaskaYearly.java
@@ -0,0 +1,72 @@
+/**
+ * Provides services for retrieving yearly flight data for Alaska flights.
+ * This class handles the process of collecting flight data over a year by breaking
+ * it down into monthly segments and then aggregating the results.
+ */
+package ca.flymile.service;
+
+import ca.flymile.ModelAlaska30Days.MonthlyDetails;
+import ca.flymile.ModelAlaska30Days.dailyCheapest;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static ca.flymile.API.RequestHandlerAlaska30Days.requestHandlerAlaska30Days;
+
+@Component
+public class AlaskaYearly {
+
+    /**
+     * Retrieves the cheapest daily flight data for an entire year from a specified origin to destination.
+     *
+     * @param origin The airport code of the origin location.
+     * @param destination The airport code of the destination location.
+     * @return A CompletableFuture that, when completed, will return a list of the cheapest daily flights for each day of the year.
+     */
+    public CompletableFuture<List<dailyCheapest>> getFlightDataListAlaskaYearly(String origin, String destination) {
+        LocalDate date = LocalDate.now().plusDays(16);
+        List<CompletableFuture<List<dailyCheapest>>> futures = new ArrayList<>();
+
+        for (int i = 0; i < 11; i++) {
+            final String start = date.toString();
+            CompletableFuture<List<dailyCheapest>> future = CompletableFuture.supplyAsync(() -> getFlightDataListAlaska30Days(origin, destination, start));
+            futures.add(future);
+            date = date.plusDays(30);
+        }
+
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenApply(v -> futures.stream()
+                        .map(CompletableFuture::join)
+                        .flatMap(List::stream)
+                        .collect(Collectors.toList()));
+    }
+
+    /**
+     * Retrieves the cheapest daily flight data for a 30-day period starting from a given date.
+     * This is used internally to gather monthly data for the yearly aggregation.
+     *
+     * @param origin The airport code of the origin location.
+     * @param destination The airport code of the destination location.
+     * @param start The start date for the 30-day period in YYYY-MM-DD format.
+     * @return A list of dailyCheapest objects representing the cheapest flights for each day of the specified period.
+     */
+    public List<dailyCheapest> getFlightDataListAlaska30Days(String origin, String destination, String start) {
+
+        // Make the API request with the adjusted start date
+        String json = requestHandlerAlaska30Days(origin, destination, start);
+        Gson gson = new Gson();
+        Type type = new TypeToken<MonthlyDetails>() {}.getType();
+
+        // Deserialize the JSON response into a MonthlyDetails object
+        MonthlyDetails monthlyDetails = gson.fromJson(json, type);
+
+        // Return the list of dailyCheapest flights from the MonthlyDetails object
+        return monthlyDetails.shoulderDates();
+    }
+}
