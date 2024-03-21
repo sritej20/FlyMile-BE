83e763addd0d45b50397f197b68990fd8a79dd83
russ.aulakh <russ.aulakh@gmail.com>
2024-03-20 23:07:44 -0400

    Update PassengerJsonProvider to optimize passenger JSON generation using a simple array approach rather than a hash map

--- src/main/java/ca/flymile/API/PassengerJsonProvider.java
+++ src/main/java/ca/flymile/API/PassengerJsonProvider.java
@@ -1,26 +1,22 @@
 package ca.flymile.API;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class PassengerJsonProvider {
 
-    private static final Map<Integer, String> passengersJsonMap = new HashMap<>();
+    private static final String[] passengersJsonMap = new String[10];
 
     static {
-        for (int i = 0; i <= 8; i++) {
+        for (int i = 1; i <= 9; i++) {
             String passengersJson = IntStream.rangeClosed(1, i)
                     .mapToObj(id -> String.format("{\"passengerTypeCode\":\"ADT\",\"passengerId\":\"%d\"}", id))
                     .collect(Collectors.joining(",", "[", "]"));
-            passengersJsonMap.put(i, passengersJson);
+            passengersJsonMap[i] = passengersJson;
 
         }
     }
 
     public static String getPassengersJson(int numPassengers) {
-        return passengersJsonMap.getOrDefault(numPassengers, "[]");
-        //Default should never happen
-        //As Passengers can only be between 1,9
+        return passengersJsonMap[numPassengers];
     }
 }
